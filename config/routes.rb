2e5dee26df80e3c370118eacb85540124cecd664
Adnan Mustafa <mirzaadnanmustafa@yahoo.com>
2024-01-30 23:03:17 +0500

    add chats view

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
+  # get 'pages/home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -9,5 +9,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   resources :chats, only: [:create, :index]
-  root "chats#index"
+  root "pages#home"
 end
